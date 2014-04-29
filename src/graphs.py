3f08d990c7b6dc6098816374c96ec0a9c1037647
Andrzej Kapanowski <ufkapano@gmail.com>
2014-04-29 17:14:29 +0200

    Graph added.

---
+++ src/graphs.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+#
+# graphs.py
+#
+# Nodes can be numbers, string, or any hashable objects.
+# We would like also to compare nodes.
+#
+# {"A":{"B":1,"C":2}, 
+# "B":{"C":3,"D":4}, 
+# "C":{"D":5}, 
+# "D":{"C":6}, 
+# "E":{"C":7}, 
+# "F":{}}
+
+from edges import Edge
+
+
+class Graph(dict):
+    """The class defining a graph."""
+
+    def __init__(self, n=0, directed=False):
+        """Loads up a Graph instance."""
+        self.n = n              # compatibility
+        self.directed = directed  # bool
+
+    def v(self):
+        """Returns the number of nodes (the graph order)."""
+        return len(self)
+
+    def e(self):
+        """Returns the number of edges."""
+        edges = sum(len(self[node]) for node in self)
+        return (edges if self.is_directed() else edges / 2)
+
+    def is_directed(self):
+        """Test if the graph is directed."""
+        return self.directed
+
+    def add_node(self, node):
+        """Add a node to the graph."""
+        if node not in self:
+            self[node] = {}
+
+    def has_node(self, node):
+        """Test if a node exists."""
+        return node in self
+
+    def del_node(self, node):
+        """Remove a node from the graph (with edges)."""
+        # dictionary changes size during iteration.
+        for edge in list(self.iterinedges(node)):
+            self.del_edge(edge)
+        if self.is_directed():
+            for edge in list(self.iteroutedges(node)):
+                self.del_edge(edge)
+        del self[node]
+
+    def add_edge(self, edge):
+        """Add an edge to the graph (missing nodes are created)."""
+        self.add_node(edge.source)
+        self.add_node(edge.target)
+        if edge.target not in self[edge.source]:
+            self[edge.source][edge.target] = edge.weight
+        if not self.is_directed():
+            if edge.source not in self[edge.target]:
+                self[edge.target][edge.source] = edge.weight
+
+    def del_edge(self, edge):
+        """Remove an edge from the graph."""
+        del self[edge.source][edge.target]
+        if not self.is_directed():
+            del self[edge.target][edge.source]
+
+    def has_edge(self, edge):
+        """Test if an edge exists."""
+        return edge.target in self[edge.source]
+
+    def weight(self, source, target):
+        """Returns the edge weight or zero."""
+        if source in self and target in self[source]:
+            return self[source][target]
+        else:
+            return 0
+
+    def iternodes(self):
+        """Generates nodes from the graph on demand."""
+        return self.iterkeys()
+
+    def iteradjacent(self, source):
+        """Generates adjacent nodes from the graph on demand."""
+        return self[source].iterkeys()
+
+    def iteroutedges(self, source):
+        """Generates outedges from the graph on demand."""
+        for target in self[source]:
+            yield Edge(source, target, self[source][target])
+
+    def iterinedges(self, source):
+        """Generates outedges from the graph on demand."""
+        if self.is_directed():   # inefficient, time O(E)
+            for edge in self.iteredges():
+                if edge.target == source:
+                    yield edge
+        else:    # iteroutedges
+            for target in self[source]:
+                yield Edge(source, target, self[source][target])
+
+    def iteredges(self):
+        """Generates edges from the graph on demand."""
+        for source in self.iternodes():
+            for target in self[source]:
+                if self.is_directed() or source < target:
+                    yield Edge(source, target, self[source][target])
+
+    def show(self):                 # propozycja wyswietlania grafu
+        for source in self.iternodes():
+            print source, ":",
+            for target in self.iteradjacent(source):
+                print "%s(%s)" % (target, self[source][target]),
+            print
+
+    def __eq__(self, other):
+        """Test if the graphs are equal."""
+        pass
+
+    def __ne__(self, other):
+        """Test if the graphs are not equal."""
+        return not self == other
+
+# EOF
---
+++ src/test_graphs.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+#
+# test_graphs.py
+#
+# Testy moga byc niezalezne od implementacji, bo jest wspolny
+# interfejs.
+
+import unittest
+from edges import Edge
+from graphs import Graph
+
+# A --> B
+# ^  /  ^
+# | /.  |
+# C --> D
+
+class TestGraphDirected(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 4           # number of nodes
+        self.G = Graph(self.N, directed=True)
+        self.edges = [Edge("A", "B", 2), Edge("B", "C", 4), Edge("C", "A", 6),
+                      Edge("C", "D", 3), Edge("D", "B", 5)]
+        for edge in self.edges:
+            self.G.add_edge(edge)
+
+    def test_directed(self):
+        self.assertTrue(self.G.is_directed())
+        self.assertEqual(self.G.v(), self.N)
+        self.assertEqual(self.G.e(), 5)
+        self.G.del_node("B")
+        self.assertEqual(self.G.e(), 2)
+
+    def tearDown(self): pass
+
+
+class TestGraphUndirected(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_undirected(self): pass
+
+    def tearDown(self): pass
+
+
+# 0-2-4-6  graf nieskierowany z wagami
+# | | | |  drabina
+# 1-3-5-7
+
+class TestGraphLadder(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 8           # number of nodes
+        self.G = Graph(self.N)
+        self.edges = [Edge(0, 1, 2), Edge(0, 2, 1), Edge(2, 3, 5),
+                      Edge(1, 3, 3), Edge(2, 4, 4), Edge(3, 5, 6), Edge(4, 6, 7),
+                      Edge(4, 5, 8), Edge(5, 7, 9), Edge(6, 7, 10)]
+        for edge in self.edges:
+            self.G.add_edge(edge)
+
+    def test_basic(self):
+        self.assertFalse(self.G.is_directed())
+        self.assertEqual(self.G.v(), self.N)
+        self.assertEqual(self.G.e(), len(self.edges))
+
+    def test_edges(self):
+        for edge in self.edges:
+            self.assertTrue(self.G.has_edge(edge))
+            self.assertEqual(self.G.weight(edge.source, edge.target), edge.weight)
+        self.assertFalse(self.G.has_edge(Edge(0, 3)))
+        self.assertEqual(self.G.weight(0, 3), 0)  # no edge
+
+    def test_del(self):
+        self.assertEqual(self.G.e(), 10)
+        self.G.del_node(7)
+        self.assertEqual(self.G.e(), 8)
+        self.G.del_node(2)
+        self.assertEqual(self.G.e(), 5)
+
+    def test_adjacent(self):
+        for node in self.G.iteradjacent(0):
+            self.assertTrue(node in [1, 2])
+        for node in self.G.iteradjacent(2):
+            self.assertTrue(node in [0, 3, 4])
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+
+    #unittest.main()
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestGraphDirected)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestGraphUndirected)
+    suite3 = unittest.TestLoader().loadTestsFromTestCase(TestGraphLadder)
+    suite = unittest.TestSuite([suite1, suite2, suite3])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+# EOF
