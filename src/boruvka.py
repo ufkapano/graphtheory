7447ae3c9050cefc337155397c778804e5b2f166
Andrzej Kapanowski <ufkapano@gmail.com>
2014-05-12 10:57:40 +0200

    Boruvka improved, total time O(E*log(V)).

--- src/boruvka.py
+++ src/boruvka.py
@@ -18,36 +18,31 @@ class BoruvkaMST:
         self.mst = Graph()   # MST is a graph
         self.uf = UnionFind()
 
-    def find_cheapest_edge(self, source):
-        """Find the cheapest edge from the component to outside."""
-        min_edge = Edge(None, None, float("inf"))
-        source = self.uf.find(source)
-        for edge in self.graph.iteredges():
-            if ((self.uf.find(edge.source) == source) 
-            == (self.uf.find(edge.target) != source)):
-                # edge to a node outside of the component
-                if edge < min_edge:
-                    min_edge = edge
-        return min_edge
-
     def run(self):
         """Executable pseudocode."""
         for node in self.graph.iternodes():
             self.uf.create(node)
         forest = set(node for node in self.graph.iternodes())
+        dummy_edge = Edge(None, None, float("inf"))
         while len(forest) > 1:
-            new_edges = []
-            for node in forest:  # total time O(V*E)
-                edge = self.find_cheapest_edge(node)
-                new_edges.append(edge)
-            # Connecting components.
-            forest = set()
-            for edge in new_edges:
+            min_edges = dict(((node, dummy_edge) for node in forest))
+            # finding the cheapest eadges
+            for edge in self.graph.iteredges(): # time O(E)
                 source = self.uf.find(edge.source)
                 target = self.uf.find(edge.target)
-                if source != target:
+                if source != target:   # different components
+                    if edge < min_edges[source]:
+                        min_edges[source] = edge
+                    if edge < min_edges[target]:
+                        min_edges[target] = edge
+            # connecting components, total time O(V)
+            forest = set()
+            for edge in min_edges.itervalues():
+                source = self.uf.find(edge.source)
+                target = self.uf.find(edge.target)
+                if source != target:   # different components
                     self.uf.union(source, target)
-                    forest.add(self.uf.find(source))
+                    forest.add(source)
                     self.mst.add_edge(edge)
             # remove duplicates, total time O(V)
             forest = set(self.uf.find(node) for node in forest)
