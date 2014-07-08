272be6fe70f15272eeedc243bbe99374ab9bf81a
Andrzej Kapanowski <ufkapano@gmail.com>
2014-07-08 15:45:33 +0200

    Tests for Johnson changed.

--- src/test_johnson.py
+++ src/test_johnson.py
@@ -11,69 +11,10 @@ class TestJohnson(unittest.TestCase):
     def setUp(self):
         self.N = 5           # number of nodes
         self.G = Graph(self.N, directed=True)
-        self.nodes = ["A", "B", "C", "D", "E"]
-        self.edges = [Edge("A", "C", 6), Edge("A", "D", 3),
-        Edge("B", "A", 3), Edge("C", "D", 2), Edge("D", "B", 1),
-        Edge("D", "C", 1), Edge("E", "B", 4), Edge("E", "D", 2)]
-        for node in self.nodes:
-            self.G.add_node(node)
-        for edge in self.edges:
-            self.G.add_edge(edge)
-
-    def test_johnson(self):
-        algorithm = Johnson(self.G)
-        algorithm.run()
-        expected_dist = dict(
-        A={'A': 0, 'C': 4, 'B': 4, 'E': float("inf"), 'D': 3},
-        B={'A': 3, 'C': 7, 'B': 0, 'E': float("inf"), 'D': 6},
-        C={'A': 6, 'C': 0, 'B': 3, 'E': float("inf"), 'D': 2},
-        D={'A': 4, 'C': 1, 'B': 1, 'E': float("inf"), 'D': 0},
-        E={'A': 6, 'C': 3, 'B': 3, 'E': 0, 'D': 2})
-        self.assertEqual(algorithm.dist, expected_dist)
-
-    def test_negative_cycle(self):
-        self.G.add_edge(Edge("B", "D", -2))
-        algorithm = Johnson(self.G)
-        self.assertRaises(ValueError, algorithm.run)
-
-class TestJohnsonNegativeEdges(unittest.TestCase):
-
-    def setUp(self):
-        self.N = 4           # number of nodes
-        self.G = Graph(self.N, directed=True)
-        self.nodes = ["A", "B", "C", "D"]
-        self.edges = [Edge("A", "B", 3), Edge("A", "C", 6),
-        Edge("B", "C", 4), Edge("B", "D", 5), Edge("C", "D", 2),
-        Edge("D", "A", -5), Edge("D", "B", -3)]
-        for node in self.nodes:
-            self.G.add_node(node)
-        for edge in self.edges:
-            self.G.add_edge(edge)
-
-    def test_johnson(self):
-        algorithm = Johnson(self.G)
-        algorithm.run()
-        expected_dist = dict(
-        A={'A': 0, 'C': 6, 'B': 3, 'D': 8}, 
-        B={'A': 0, 'C': 4, 'B': 0, 'D': 5}, 
-        C={'A': -3, 'C': 0, 'B': -1, 'D': 2},
-        D={'A': -5, 'C': 1, 'B': -3, 'D': 0})
-        self.assertEqual(algorithm.dist, expected_dist)
-
-    def test_negative_cycle(self):
-        self.G.add_edge(Edge("A", "D", 2))
-        algorithm = Johnson(self.G)
-        self.assertRaises(ValueError, algorithm.run)
-
-class TestJohnsonWiki(unittest.TestCase):
-
-    def setUp(self):
-        self.N = 4           # number of nodes
-        self.G = Graph(self.N, directed=True)
-        self.nodes = ["x", "y", "z", "w"]
-        self.edges = [Edge("x", "y", 3), Edge("x", "w", 6),
-        Edge("y", "w", 4), Edge("y", "z", 5), Edge("w", "z", 2),
-        Edge("z", "x", -7), Edge("z", "y", -3)]
+        self.nodes = [0, 1, 2, 3, 4]
+        self.edges = [Edge(0, 2, 6), Edge(0, 3, 3),
+        Edge(1, 0, 3), Edge(2, 3, 2), Edge(3, 1, 1),
+        Edge(3, 2, 1), Edge(4, 1, 4), Edge(4, 3, 2)]
         for node in self.nodes:
             self.G.add_node(node)
         for edge in self.edges:
@@ -83,13 +24,72 @@ class TestJohnsonWiki(unittest.TestCase):
         algorithm = Johnson(self.G)
         algorithm.run()
         expected_dist = {
-        'y': {'y': 0, 'x': -2, 'z': 5, 'w': 4}, 
-        'x': {'y': 3, 'x': 0, 'z': 8, 'w': 6}, 
-        'z': {'y': -4, 'x': -7, 'z': 0, 'w': -1}, 
-        'w': {'y': -2, 'x': -5, 'z': 2, 'w': 0}}
+        0: {0: 0, 2: 4, 1: 4, 4: float('inf'), 3: 3},
+        1: {0: 3, 2: 7, 1: 0, 4: float('inf'), 3: 6},
+        2: {0: 6, 2: 0, 1: 3, 4: float('inf'), 3: 2},
+        3: {0: 4, 2: 1, 1: 1, 4: float('inf'), 3: 0},
+        4: {0: 6, 2: 3, 1: 3, 4: 0, 3: 2}}
         self.assertEqual(algorithm.dist, expected_dist)
 
-if __name__ == "__main__":
+    def test_negative_cycle(self):
+        self.G.add_edge(Edge(1, 3, -2))
+        algorithm = Johnson(self.G)
+        self.assertRaises(ValueError, algorithm.run)
+
+class TestJohnsonNegativeEdges(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 4           # number of nodes
+        self.G = Graph(self.N, directed=True)
+        self.nodes = [0, 1, 2, 3]
+        self.edges = [Edge(0, 1, 3), Edge(0, 2, 6),
+        Edge(1, 2, 4), Edge(1, 3, 5), Edge(2, 3, 2),
+        Edge(3, 0, -5), Edge(3, 1, -3)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+
+    def test_johnson(self):
+        algorithm = Johnson(self.G)
+        algorithm.run()
+        expected_dist = {
+        0: {0: 0, 2: 6, 1: 3, 3: 8}, 
+        1: {0: 0, 2: 4, 1: 0, 3: 5}, 
+        2: {0: -3, 2: 0, 1: -1, 3: 2},
+        3: {0: -5, 2: 1, 1: -3, 3: 0}}
+        self.assertEqual(algorithm.dist, expected_dist)
+
+    def test_negative_cycle(self):
+        self.G.add_edge(Edge(0, 3, 2))
+        algorithm = Johnson(self.G)
+        self.assertRaises(ValueError, algorithm.run)
+
+class TestJohnsonWiki(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 4           # number of nodes
+        self.G = Graph(self.N, directed=True)
+        self.nodes = [0, 1, 2, 3]
+        self.edges = [Edge(0, 1, 3), Edge(0, 3, 6),
+        Edge(1, 3, 4), Edge(1, 2, 5), Edge(3, 2, 2),
+        Edge(2, 0, -7), Edge(2, 1, -3)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+
+    def test_johnson(self):
+        algorithm = Johnson(self.G)
+        algorithm.run()
+        expected_dist = {
+        1: {1: 0, 0: -2, 2: 5, 3: 4}, 
+        0: {1: 3, 0: 0, 2: 8, 3: 6}, 
+        2: {1: -4, 0: -7, 2: 0, 3: -1}, 
+        3: {1: -2, 0: -5, 2: 2, 3: 0}}
+        self.assertEqual(algorithm.dist, expected_dist)
+
+if __name__ == '__main__':
 
     #unittest.main()
     suite1 = unittest.TestLoader().loadTestsFromTestCase(TestJohnson)
