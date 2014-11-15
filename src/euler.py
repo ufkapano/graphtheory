ab7e6ea0c28a0d8938a54245ddb7970a4c280637
Andrzej Kapanowski <ufkapano@gmail.com>
2014-11-15 16:31:21 +0100

    Hierholzer added (Eulerian cycle).

--- src/euler.py
+++ src/euler.py
@@ -51,4 +51,52 @@ class EulerianCycleDFS:
                     return False
         return True
 
+
+class Hierholzer:
+    """Finding an Eulerian cycle."""
+
+    def __init__(self, multigraph):
+        """The algorithm initialization."""
+        self.multigraph = multigraph
+        if not self._is_eulerian():
+            raise ValueError("the multigraph is not eulerian")
+        self.eulerian_cycle = list()
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        if source is None:   # get first random node
+            source = self.multigraph.iternodes().next()
+        self.multigraph_copy = self.multigraph.copy()
+        self.stack = LifoQueue()
+        self.eulerian_cycle.append(source)
+        while True:
+            if self.multigraph_copy.outdegree(source) > 0:
+                edge = self.multigraph_copy.iteroutedges(source).next()
+                self.stack.put(source)
+                self.multigraph_copy.del_edge(edge)
+                source = edge.target
+            else:
+                source = self.stack.get()
+                self.eulerian_cycle.append(source)
+            if self.stack.empty():
+                break
+        self.eulerian_cycle.reverse()
+        self.eulerian_cycle.pop()
+        del self.stack
+        del self.multigraph_copy
+
+    def _is_eulerian(self):
+        """Test if the multigraph is eulerian."""
+        if self.multigraph.is_directed():
+            # we assume that the multigraph is strongly connected
+            for node in self.multigraph.iternodes():
+                if self.multigraph.indegree(node) != self.multigraph.outdegree(node):
+                    return False
+        else:
+            # we assume that the multigraph is connected
+            for node in self.multigraph.iternodes():
+                if self.multigraph.degree(node) % 2 == 1:
+                    return False
+        return True
+
 # EOF
--- src/test_euler.py
+++ src/test_euler.py
@@ -3,7 +3,7 @@
 import unittest
 from edges import Edge
 from graphs import Graph
-from euler import EulerianCycleDFS
+from euler import EulerianCycleDFS, Hierholzer
 
 # 0 --- 1     2
 # |     |   / |
@@ -26,6 +26,13 @@ class TestEulerianCycleUndirected(unittest.TestCase):
 
     def test_euler_dfs(self):
         algorithm = EulerianCycleDFS(self.G)
+        algorithm.run(0)
+        expected_cycle = [0, 1, 4, 2, 5, 4, 3]
+        self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
+        self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
+
+    def test_hierholzer(self):
+        algorithm = Hierholzer(self.G)
         algorithm.run(0)
         expected_cycle = [0, 1, 4, 2, 5, 4, 3]
         self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
@@ -68,6 +75,13 @@ class TestEulerianCycleUndirected2(unittest.TestCase):
         self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
         self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
 
+    def test_hierholzer(self):
+        algorithm = Hierholzer(self.G)
+        algorithm.run(6)
+        expected_cycle = [6, 4, 0, 1, 4, 5, 2, 3, 5]
+        self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
+        self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
+
     def test_eulerian(self):
         self.G.add_edge(Edge(1, 2))
         self.assertRaises(ValueError, EulerianCycleDFS, self.G)
@@ -97,6 +111,57 @@ class TestEulerianCycleDirected(unittest.TestCase):
         algorithm = EulerianCycleDFS(self.G)
         algorithm.run(0)
         expected_cycle = [0, 1, 4, 5, 2, 4, 3]
+        self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
+        self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
+
+    def test_hierholzer(self):
+        algorithm = Hierholzer(self.G)
+        algorithm.run(0)
+        expected_cycle = [0, 1, 4, 5, 2, 4, 3]
+        self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
+        self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
+
+    def test_eulerian(self):
+        self.G.add_edge(Edge(1, 2))
+        self.assertRaises(ValueError, EulerianCycleDFS, self.G)
+
+    def tearDown(self): pass
+
+# 0 --o 1     2 --o 3
+#   o   |     o   /
+#    \  |     |  /
+#     \ o     | o
+#       4 --o 5
+#       o   /
+#       |  /
+#       | o
+#       6
+
+class TestEulerianCycleDirected2(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 7           # number of nodes
+        self.G = Graph(self.N, directed=True)
+        self.nodes = [0, 1, 2, 3, 4, 5, 6]
+        self.edges = [Edge(0, 1), Edge(4, 0), Edge(1, 4),
+            Edge(2, 3), Edge(5, 2), Edge(3, 5), Edge(4, 5),
+            Edge(6, 4), Edge(5, 6)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+
+    def test_euler_dfs(self):
+        algorithm = EulerianCycleDFS(self.G)
+        algorithm.run(6)
+        expected_cycle = [6, 4, 0, 1, 4, 5, 2, 3, 5]
+        self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
+        self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
+
+    def test_hierholzer(self):
+        algorithm = Hierholzer(self.G)
+        algorithm.run(6)
+        expected_cycle = [6, 4, 0, 1, 4, 5, 2, 3, 5]
         self.assertEqual(len(algorithm.eulerian_cycle), len(self.edges))
         self.assertEqual(algorithm.eulerian_cycle, expected_cycle)
 
