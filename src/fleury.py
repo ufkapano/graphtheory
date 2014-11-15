ff1597efa9ceb4976ccdd650a602008ca48a5ba0
Andrzej Kapanowski <ufkapano@gmail.com>
2014-11-15 16:34:58 +0100

    Fleury for multigraphs.

--- src/fleury.py
+++ src/fleury.py
@@ -7,54 +7,54 @@ from bfs import SimpleBFS
 class FleuryDFS:
     """Fleury's algorithm for finding an Eulerian cycle."""
 
-    def __init__(self, graph):
+    def __init__(self, multigraph):
         """The algorithm initialization."""
-        self.graph = graph
+        self.multigraph = multigraph
         if not self._is_eulerian():
-            raise ValueError("the graph is not eulerian")
+            raise ValueError("the multigraph is not eulerian")
         self.eulerian_cycle = list()
 
     def run(self, source=None):
         """Executable pseudocode."""
         if source is None:   # get first random node
-            source = self.graph.iternodes().next()
+            source = self.multigraph.iternodes().next()
         node = source
         self.eulerian_cycle.append(node)
-        self.graph_copy = self.graph.copy()
-        while self.graph_copy.outdegree(node) > 0:
-            for edge in list(self.graph_copy.iteroutedges(node)):
-                # graph_copy is changing!
+        self.multigraph_copy = self.multigraph.copy()
+        while self.multigraph_copy.outdegree(node) > 0:
+            for edge in list(self.multigraph_copy.iteroutedges(node)):
+                # multigraph_copy is changing!
                 if not self._is_bridge(edge):
                     break
-            self.graph_copy.del_edge(edge)
+            self.multigraph_copy.del_edge(edge)
             self.eulerian_cycle.append(edge.target)
             node = edge.target
-        del self.graph_copy
+        del self.multigraph_copy
         self.eulerian_cycle.pop()
 
     def _is_bridge(self, edge):
         """Bridge test."""
         list1 = list()
         list2 = list()
-        algorithm = SimpleDFS(self.graph_copy)
+        algorithm = SimpleDFS(self.multigraph_copy)
         algorithm.run(edge.source, pre_action=lambda node: list1.append(node))
-        self.graph_copy.del_edge(edge)
-        algorithm = SimpleDFS(self.graph_copy)
+        self.multigraph_copy.del_edge(edge)
+        algorithm = SimpleDFS(self.multigraph_copy)
         algorithm.run(edge.source, pre_action=lambda node: list2.append(node))
-        self.graph_copy.add_edge(edge)
+        self.multigraph_copy.add_edge(edge)
         return len(list1) != len(list2)
 
     def _is_eulerian(self):
-        """Test if the graph is eulerian."""
-        if self.graph.is_directed():
-            # We assume that the graph is strongly connected.
-            for node in self.graph.iternodes():
-                if self.graph.indegree(node) != self.graph.outdegree(node):
+        """Test if the multigraph is eulerian."""
+        if self.multigraph.is_directed():
+            # We assume that the multigraph is strongly connected.
+            for node in self.multigraph.iternodes():
+                if self.multigraph.indegree(node) != self.multigraph.outdegree(node):
                     return False
         else:
-            # We assume that the graph is connected.
-            for node in self.graph.iternodes():
-                if self.graph.degree(node) % 2 == 1:
+            # We assume that the multigraph is connected.
+            for node in self.multigraph.iternodes():
+                if self.multigraph.degree(node) % 2 == 1:
                     return False
         return True
 
@@ -62,54 +62,54 @@ class FleuryDFS:
 class FleuryBFS:
     """Fleury's algorithm for finding an Eulerian cycle."""
 
-    def __init__(self, graph):
+    def __init__(self, multigraph):
         """The algorithm initialization."""
-        self.graph = graph
+        self.multigraph = multigraph
         if not self._is_eulerian():
-            raise ValueError("the graph is not eulerian")
+            raise ValueError("the multigraph is not eulerian")
         self.eulerian_cycle = list()
 
     def run(self, source=None):
         """Executable pseudocode."""
         if source is None:   # get first random node
-            source = self.graph.iternodes().next()
+            source = self.multigraph.iternodes().next()
         node = source
         self.eulerian_cycle.append(node)
-        self.graph_copy = self.graph.copy()
-        while self.graph_copy.outdegree(node) > 0:
-            for edge in list(self.graph_copy.iteroutedges(node)):
-                # graph_copy is changing!
+        self.multigraph_copy = self.multigraph.copy()
+        while self.multigraph_copy.outdegree(node) > 0:
+            for edge in list(self.multigraph_copy.iteroutedges(node)):
+                # multigraph_copy is changing!
                 if not self._is_bridge(edge):
                     break
-            self.graph_copy.del_edge(edge)
+            self.multigraph_copy.del_edge(edge)
             self.eulerian_cycle.append(edge.target)
             node = edge.target
-        del self.graph_copy
+        del self.multigraph_copy
         self.eulerian_cycle.pop()
 
     def _is_bridge(self, edge):
         """Bridge test."""
         list1 = list()
         list2 = list()
-        algorithm = SimpleBFS(self.graph_copy)
+        algorithm = SimpleBFS(self.multigraph_copy)
         algorithm.run(edge.source, pre_action=lambda node: list1.append(node))
-        self.graph_copy.del_edge(edge)
-        algorithm = SimpleBFS(self.graph_copy)
+        self.multigraph_copy.del_edge(edge)
+        algorithm = SimpleBFS(self.multigraph_copy)
         algorithm.run(edge.source, pre_action=lambda node: list2.append(node))
-        self.graph_copy.add_edge(edge)
+        self.multigraph_copy.add_edge(edge)
         return len(list1) != len(list2)
 
     def _is_eulerian(self):
-        """Test if the graph is eulerian."""
-        if self.graph.is_directed():
-            # We assume that the graph is strongly connected.
-            for node in self.graph.iternodes():
-                if self.graph.indegree(node) != self.graph.outdegree(node):
+        """Test if the multigraph is eulerian."""
+        if self.multigraph.is_directed():
+            # We assume that the multigraph is strongly connected.
+            for node in self.multigraph.iternodes():
+                if self.multigraph.indegree(node) != self.multigraph.outdegree(node):
                     return False
         else:
-            # We assume that the graph is connected.
-            for node in self.graph.iternodes():
-                if self.graph.degree(node) % 2 == 1:
+            # We assume that the multigraph is connected.
+            for node in self.multigraph.iternodes():
+                if self.multigraph.degree(node) % 2 == 1:
                     return False
         return True
 
