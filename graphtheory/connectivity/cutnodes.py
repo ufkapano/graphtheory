4762b9e4c684cd97a39423637c76763ed512b749
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2015-11-18 15:32:05 +0100

    graphtheory.connectivity.cutnodes added.

---
+++ graphtheory/connectivity/cutnodes.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+
+from graphtheory.traversing.dfs import SimpleDFS
+
+
+class TrivialCutNode:
+    """Trivial algorithm for finding cut nodes (articulation points).
+    
+    Attributes
+    ----------
+    graph : input undirected graph
+    cut_nodes : list of nodes
+    
+    Notes
+    -----
+    Based on the description from:
+    
+    http://eduinf.waw.pl/inf/alg/001_search/0130b.php
+    
+    https://en.wikipedia.org/wiki/Connectivity_(graph_theory)
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("graph is directed")
+        self.graph = graph
+        self.cut_nodes = list()
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        old_ncc = self._find_ncc()
+        for source in self.graph.iternodes():
+            removed = list(self.graph.iteroutedges(source))
+            for edge in removed:
+                self.graph.del_edge(edge)
+            new_ncc = self._find_ncc()
+            for edge in removed:
+                self.graph.add_edge(edge)
+            if new_ncc > old_ncc + 1:   # source was not removed
+                self.cut_nodes.append(source)
+
+    def _find_ncc(self):
+        """Return the number of connected components."""
+        visited = dict((node, False) for node in self.graph.iternodes())
+        ncc = 0
+        algorithm = SimpleDFS(self.graph)
+        for source in self.graph.iternodes():
+            if not visited[source]:
+                algorithm.run(source, pre_action=lambda node:
+                    visited.__setitem__(node, True))
+                ncc = ncc + 1
+        return ncc
+
+
+class TarjanCutNode:
+    """Tarjan's algorithm for finding cut nodes in O(V+E) time.
+    
+    Attributes
+    ----------
+    graph : input undirected graph
+    cut_nodes : list of nodes
+    low : dict with nodes
+    parent : dict with nodes (DFS tree)
+    _time : number, private
+    _dd : dict with nodes (values are numbers, time stamps), private
+    dag : graph (DFS tree)
+    
+    Notes
+    -----
+    Based on the description from:
+    
+    http://eduinf.waw.pl/inf/alg/001_search/0130b.php
+    
+    https://en.wikipedia.org/wiki/Connectivity_(graph_theory)
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("graph is directed")
+        self.graph = graph
+        self.cut_nodes = list()
+        # Parametr dla wierzcholka wprowadzony przez Tarjana.
+        self.low = dict(((node, None) for node in self.graph.iternodes()))
+        self.parent = dict(((node, None) for node in self.graph.iternodes()))
+        self._time = 0    # time stamp
+        self._dd = dict(((node, 0) for node in self.graph.iternodes()))
+        self.dag = self.graph.__class__(self.graph.v(), directed=True)
+        import sys
+        recursionlimit = sys.getrecursionlimit()
+        sys.setrecursionlimit(max(self.graph.v() * 2, recursionlimit))
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        if source is not None:
+            self._visit_root(source)
+        else:
+            for node in self.graph.iternodes():
+                if self._dd[node] == 0:   # not visited
+                    self._visit_root(node)
+
+    def _visit_root(self, node):
+        """Explore recursively the connected component from root."""
+        self._time = self._time + 1
+        self._dd[node] = self._time
+        self.low[node] = self._time
+        n_sons = 0   # number of sons for the root
+        for edge in self.graph.iteroutedges(node):
+            if self._dd[edge.target] == 0:   # not visited
+                n_sons = n_sons + 1
+                self.parent[edge.target] = node
+                self.dag.add_edge(edge)
+                self._visit(edge.target)
+        # Is the root an articulation point?
+        if n_sons > 1:
+            self.cut_nodes.append(node)
+
+    def _visit(self, node):
+        """Explore recursively the connected component."""
+        self._time = self._time + 1
+        self._dd[node] = self._time
+        self.low[node] = self._time   # temporary
+        is_cut_node = False
+        for edge in self.graph.iteroutedges(node):
+            if edge.target == self.parent[node]:   # can be None!
+                continue
+            if self._dd[edge.target] == 0:   # not visited
+                self.parent[edge.target] = node
+                self.dag.add_edge(edge)
+                self._visit(edge.target)
+                self.low[node] = min(self.low[node], self.low[edge.target])
+                # Test for an articulation point.
+                if self.low[edge.target] >= self._dd[node]:
+                    is_cut_node = True
+            else:   # back edge
+                self.low[node] = min(self.low[node], self._dd[edge.target])
+        # All neighbors are visited. Check the test.
+        if is_cut_node:
+            self.cut_nodes.append(node)
+
+# EOF
---
+++ graphtheory/connectivity/tests/test_cutnodes.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.connectivity.cutnodes import TrivialCutNode, TarjanCutNode
+
+# 0---1   2---3
+# | / | / | \ |
+# 4   5---6   7
+
+class TestCutNode(unittest.TestCase):
+
+    def setUp(self):
+        # The graph from Cormen p.607 changed.
+        self.N = 8           # number of nodes
+        self.G = Graph(self.N)
+        self.nodes = range(self.N)
+        self.edges = [Edge(0, 4, 2), Edge(0, 1, 3), Edge(1, 4, 12),
+            Edge(1, 5, 4), Edge(5, 2, 5), Edge(5, 6, 6), Edge(2, 6, 7),
+            Edge(2, 3, 8), Edge(2, 7, 9), Edge(3, 7, 11)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+        #print self.G
+        #self.G.show()
+
+    def test_trivial_cut_node(self):
+        algorithm = TrivialCutNode(self.G)
+        algorithm.run(0)
+        cut_nodes_expected = [1, 2, 5]
+        self.assertEqual(algorithm.cut_nodes, cut_nodes_expected)
+
+    def test_tarjan_cut_node(self):
+        algorithm = TarjanCutNode(self.G)
+        algorithm.run(0)
+        cut_nodes_expected = [2, 5, 1]
+        self.assertEqual(algorithm.cut_nodes, cut_nodes_expected)
+        dd_expected = {0: 1, 1: 2, 2: 5, 3: 6, 4: 3, 5: 4, 6: 8, 7: 7}
+        self.assertEqual(algorithm._dd, dd_expected)
+        parent_expected = {0: None, 1: 0, 2: 5, 3: 2, 4: 1, 5: 1, 6: 2, 7: 3}
+        self.assertEqual(algorithm.parent, parent_expected)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
