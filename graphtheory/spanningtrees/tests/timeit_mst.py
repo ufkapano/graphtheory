b6f82a1720a12be6e811d4c1d178b854c4c166b1
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2016-03-18 17:52:23 +0100

    Using timeit for MST.

---
+++ graphtheory/spanningtrees/tests/timeit_mst.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import timeit
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.structures.factory import GraphFactory
+from graphtheory.connectivity.connected import is_connected
+from graphtheory.algorithms.acyclic import is_acyclic
+from graphtheory.bipartiteness.bipartite import is_bipartite
+from graphtheory.spanningtrees.boruvka import BoruvkaMST
+from graphtheory.spanningtrees.prim import PrimMST
+from graphtheory.spanningtrees.prim import PrimMatrixMST
+from graphtheory.spanningtrees.prim import PrimTrivialMST
+from graphtheory.spanningtrees.kruskal import KruskalMST
+
+V = 10
+graph_factory = GraphFactory(Graph)
+G = graph_factory.make_random(V, False, 0.5)
+#G = graph_factory.make_complete(V, False)
+#G = graph_factory.make_cyclic(V, False)
+E = G.e()
+#G.show()
+
+print "Calculate parameters ..."
+print "Nodes:", G.v(), V
+print "Edges:", G.e(), E
+print "Directed:", G.is_directed()
+print "Connected:", is_connected(G)
+print "Acyclic:", is_acyclic(G)
+print "Bipartite:", is_bipartite(G)
+Delta = max(G.degree(node) for node in G.iternodes())
+print "Delta:", Delta
+
+print "Testing BoruvkaMST ..."
+t1 = timeit.Timer(lambda: BoruvkaMST(G).run())
+print V, E, t1.timeit(1)            # single run
+
+print "Testing PrimMST ..."
+t1 = timeit.Timer(lambda: PrimMST(G).run())
+print V, E, t1.timeit(1)            # single run
+
+print "Testing PrimMatrixMST ..."
+t1 = timeit.Timer(lambda: PrimMatrixMST(G).run())
+print V, E, t1.timeit(1)            # single run
+
+print "Testing PrimTrivialMST ..."
+t1 = timeit.Timer(lambda: PrimTrivialMST(G).run())
+print V, E, t1.timeit(1)            # single run
+
+print "Testing KruskalMST ..."
+t1 = timeit.Timer(lambda: KruskalMST(G).run())
+print V, E, t1.timeit(1)            # single run
+
+# EOF
