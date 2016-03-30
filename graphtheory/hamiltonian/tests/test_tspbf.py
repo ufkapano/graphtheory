1cbd7261bbe972bf57c9d1d0db254a461b1e1b4b
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2016-03-30 09:37:09 +0200

    The brute force algorithm for TSP added.

---
+++ graphtheory/hamiltonian/tests/test_tspbf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.hamiltonian.tspbf import *
+
+# http://en.wikipedia.org/wiki/Travelling_salesman_problem
+# Wagi krawedzi spelniaja warunek trojkata.
+#      20                Three Hamiltonian cycles:
+#   0 --- 1              0 1 3 2 = 108
+#   | \ / |              0 2 1 3 = 141
+# 42|  X  |34            0 1 2 3 = 97
+#   | / \ |
+#   2 --- 3
+#      12
+
+class TestTSP(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 4
+        self.G = Graph(self.N)
+        self.nodes = range(self.N)
+        self.edges = [
+            Edge(0, 1, 20), Edge(0, 3, 35), Edge(0, 2, 42), 
+            Edge(1, 2, 30), Edge(1, 3, 34), Edge(2, 3, 12)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+        self.best_weight = 97
+        #self.G.show()
+
+    def test_brute_force_with_edges(self):
+        algorithm = BruteForceTSPWithEdges(self.G)
+        algorithm.run(2)
+        # Cycles for different starting nodes.
+        expected_hamiltonian_cycle0 = [Edge(0, 1, 20), Edge(1, 2, 30), Edge(2, 3, 12), Edge(3, 0, 35)]
+        expected_hamiltonian_cycle1 = [Edge(1, 0, 20), Edge(0, 3, 35), Edge(3, 2, 12), Edge(2, 1, 30)]
+        expected_hamiltonian_cycle2 = [Edge(2, 1, 30), Edge(1, 0, 20), Edge(0, 3, 35), Edge(3, 2, 12)]
+        expected_hamiltonian_cycle3 = [Edge(3, 0, 35), Edge(0, 1, 20), Edge(1, 2, 30), Edge(2, 3, 12)]
+        expected_hamiltonian_cycle = expected_hamiltonian_cycle2
+        self.assertEqual(algorithm.hamiltonian_cycle, expected_hamiltonian_cycle)
+        weight = sum(edge.weight for edge in algorithm.hamiltonian_cycle)
+        self.assertEqual(weight, self.best_weight)
+
+    def test_brute_force_with_cycle_graph(self):
+        algorithm = BruteForceTSPWithGraph(self.G)
+        algorithm.run(2)
+        # Hamiltonian cycle as a graph.
+        weight = sum(edge.weight for edge in algorithm.hamiltonian_cycle.iteredges())
+        self.assertEqual(weight, self.best_weight)
+        #algorithm.hamiltonian_cycle.show()
+        self.assertEqual(algorithm.hamiltonian_cycle.e(), algorithm.hamiltonian_cycle.v())
+        for node in algorithm.hamiltonian_cycle.iternodes():
+            self.assertEqual(algorithm.hamiltonian_cycle.degree(node), 2)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
--- graphtheory/hamiltonian/tspbf.py
+++ graphtheory/hamiltonian/tspbf.py
@@ -1,87 +1,29 @@
 #!/usr/bin/python
 
-class HamiltonianCycleDFS:
-    """Finding a Hamiltonian cycle in a Hamiltonian graph.
+
+class BruteForceTSPWithEdges:
+    """The brute force algorithm (a.k.a. exhaustive search) for TSP.
     
     Attributes
     ----------
-    graph : input graph
-    hamiltonian_cycle : list of nodes
-    source : starting node
-    _stack : list of nodes, private
-    _used : dict with nodes, private
-    
-    Notes
-    -----
-    Based on the description from:
-    
-    http://edu.i-lo.tarnow.pl/inf/alg/001_search/0136.php
-    """
-
-    def __init__(self, graph):
-        """The algorithm initialization."""
-        self.graph = graph
-        self.hamiltonian_cycle = list()
-        self.source = None
-        self._stack = list()
-        self._used = dict((node, False) for node in self.graph.iternodes())
-        import sys
-        recursionlimit = sys.getrecursionlimit()
-        sys.setrecursionlimit(max(self.graph.v() * 2, recursionlimit))
-
-    def run(self, source=None):
-        """Executable pseudocode."""
-        if source is None:   # get first random node
-            source = self.graph.iternodes().next()
-        self.source = source
-        self._hamilton_dfs(self.source)
-
-    def _hamilton_dfs(self, node):
-        """Modified DFS from the node."""
-        if self.hamiltonian_cycle:
-            return
-        self._stack.append(node)   # at the beginning of _hamilton_dfs
-        if len(self._stack) == self.graph.v():
-            # Hamiltonian path is possible.
-            for edge in self.graph.iteroutedges(node):
-                if edge.target == self.source:
-                    self._stack.append(self.source)
-                    self.hamiltonian_cycle = list(self._stack)
-                    self._stack.pop()
-        else:
-            self._used[node] = True
-            for edge in self.graph.iteroutedges(node):
-                if not self._used[edge.target]:
-                    self._hamilton_dfs(edge.target)
-            self._used[node] = False
-        self._stack.pop()   # at the end of _hamilton_dfs
-
-
-class HamiltonianCycleDFSWithEdges:
-    """Finding a Hamiltonian cycle in a Hamiltonian graph.
-    
-    Attributes
-    ----------
-    graph : input graph
+    graph : input weighted complete graph
     hamiltonian_cycle : list of edges
     source : starting node
+    best_weight : number
     _stack : list of nodes, private
     _used : dict with nodes, private
-    
-    Notes
-    -----
-    Based on the description from:
-    
-    http://edu.i-lo.tarnow.pl/inf/alg/001_search/0136.php
     """
 
     def __init__(self, graph):
         """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
         self.graph = graph
         self.hamiltonian_cycle = list()
         self.source = None
         self._stack = list()
         self._used = dict((node, False) for node in self.graph.iternodes())
+        self.best_weight = float("inf")
         import sys
         recursionlimit = sys.getrecursionlimit()
         sys.setrecursionlimit(max(self.graph.v() * 2, recursionlimit))
@@ -95,14 +37,16 @@ class HamiltonianCycleDFSWithEdges:
 
     def _hamilton_dfs(self, node):
         """Modified DFS from the node."""
-        if self.hamiltonian_cycle:
-            return
         if len(self._stack) == self.graph.v()-1:
             # Hamiltonian path is possible.
             for edge in self.graph.iteroutedges(node):
                 if edge.target == self.source:
+                    # We have a Hamiltonian cycle.
                     self._stack.append(edge)
-                    self.hamiltonian_cycle = list(self._stack)
+                    weight = sum(edge.weight for edge in self._stack)
+                    if weight < self.best_weight:
+                        self.best_weight = weight
+                        self.hamiltonian_cycle = list(self._stack)
                     self._stack.pop()
         else:
             self._used[node] = True
@@ -114,26 +58,23 @@ class HamiltonianCycleDFSWithEdges:
             self._used[node] = False
 
 
-class HamiltonianCycleDFSWithGraph:
-    """Finding a Hamiltonian cycle in a Hamiltonian graph.
+class BruteForceTSPWithGraph:
+    """The brute force algorithm (a.k.a. exhaustive search) for TSP.
     
     Attributes
     ----------
-    graph : input graph
+    graph : input weighted complete graph
     hamiltonian_cycle : cycle graph
     source : starting node
+    best_weight : number
     _path : graph, private
     _used : dict with nodes, private
-    
-    Notes
-    -----
-    Based on the description from:
-    
-    http://edu.i-lo.tarnow.pl/inf/alg/001_search/0136.php
     """
 
     def __init__(self, graph):
         """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
         self.graph = graph
         self.hamiltonian_cycle = None
         self.source = None
@@ -141,6 +82,7 @@ class HamiltonianCycleDFSWithGraph:
         for node in self.graph.iternodes():
             self._path.add_node(node)
         self._used = dict((node, False) for node in self.graph.iternodes())
+        self.best_weight = float("inf")
         import sys
         recursionlimit = sys.getrecursionlimit()
         sys.setrecursionlimit(max(self.graph.v() * 2, recursionlimit))
@@ -154,14 +96,16 @@ class HamiltonianCycleDFSWithGraph:
 
     def _hamilton_dfs(self, node):
         """Modified DFS from the node."""
-        if self.hamiltonian_cycle:
-            return
         if self._path.e() == self.graph.v()-1:
             # Hamiltonian path is possible.
             for edge in self.graph.iteroutedges(node):
                 if edge.target == self.source:
+                    # We have a Hamiltonian cycle.
                     self._path.add_edge(edge)
-                    self.hamiltonian_cycle = self._path.copy()
+                    weight = sum(edge.weight for edge in self._path.iteredges())
+                    if weight < self.best_weight:
+                        self.best_weight = weight
+                        self.hamiltonian_cycle = self._path.copy()
                     self._path.del_edge(edge)
         else:
             self._used[node] = True
