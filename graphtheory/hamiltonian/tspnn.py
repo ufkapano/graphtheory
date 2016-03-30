268048d1652b5849a5b2d5b3cd6f4725f002339d
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2016-03-30 09:59:47 +0200

    Nearest Neighbor algorithm for TSP added.

---
+++ graphtheory/hamiltonian/tspnn.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+class NearestNeighborTSPWithEdges:
+    """The nearest neighbor algorithm for TSP.
+    
+    Attributes
+    ----------
+    graph : input weighted complete graph
+    hamiltonian_cycle : list of edges
+    source : starting node
+    _used : dict with nodes, private
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.hamiltonian_cycle = list()
+        self.source = None
+        self._used = dict((node, False) for node in self.graph.iternodes())
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        if source is None:
+            source = self.graph.iternodes().next()
+        start_node = source
+        self._used[source] = True
+        # Szukamy n-1 kolejnych krawedzi. Unikamy tworzenia cyklu.
+        for step in xrange(self.graph.v()-1):
+            edge = min(edge for edge in self.graph.iteroutedges(source)
+                if not self._used[edge.target])
+            self.hamiltonian_cycle.append(edge)
+            self._used[edge.target] = True
+            source = edge.target
+        # We have to close the cycle.
+        for edge in self.graph.iteroutedges(source):
+            if edge.target == start_node:
+                self.hamiltonian_cycle.append(edge)
+                break
+
+
+class NearestNeighborTSPWithGraph:
+    """The nearest neighbor algorithm for TSP."""
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.hamiltonian_cycle = self.graph.__class__(self.graph.v(), self.graph.is_directed())
+        for node in self.graph.iternodes():
+            self.hamiltonian_cycle.add_node(node)
+        self.source = None
+        self._used = dict((node, False) for node in self.graph.iternodes())
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        if source is None:
+            source = self.graph.iternodes().next()
+        self.source = source
+        self._used[self.source] = True
+        # Szukamy n-1 kolejnych krawedzi. Unikamy tworzenia cyklu.
+        for step in xrange(self.graph.v()-1):
+            edge = min(edge for edge in self.graph.iteroutedges(source)
+                if not self._used[edge.target])
+            self.hamiltonian_cycle.add_edge(edge)
+            self._used[edge.target] = True
+            source = edge.target
+        # We have to close the cycle.
+        for edge in self.graph.iteroutedges(source):
+            if edge.target == self.source:
+                self.hamiltonian_cycle.add_edge(edge)
+                break
+
+# EOF
