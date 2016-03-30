56bc2ac53a36acdd8ab16860f6b25e6ab8558e05
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2016-03-30 10:58:27 +0200

    Sorted Edge algorithm for TSP added.

---
+++ graphtheory/hamiltonian/tests/test_tspse.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.hamiltonian.tspse import *
+
+# http://en.wikipedia.org/wiki/Travelling_salesman_problem
+# Wagi krawedzi spelniaja warunek trojkata.
+#      20                Three Hamiltonian cycles:
+#   0 --- 1              0 1 3 2 = 108
+#   | \ / |              0 2 1 3 = 141
+# 42|  X  |34            0 1 2 3 = 97
+#   | / \ |
+#   2 --- 3
+#      12
+
+class TestTSP(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 4
+        self.G = Graph(self.N)
+        self.nodes = range(self.N)
+        self.edges = [
+            Edge(0, 1, 20), Edge(0, 3, 35), Edge(0, 2, 42), 
+            Edge(1, 2, 30), Edge(1, 3, 34), Edge(2, 3, 12)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+        self.best_weight = 97
+        #self.G.show()
+
+    def test_sorted_edge_with_edges(self):
+        algorithm = SortedEdgeTSPWithEdges(self.G)
+        algorithm.run(0)
+        expected_hamiltonian_cycle = [
+            Edge(2, 3, 12), Edge(3, 0, 35), Edge(0, 1, 20), Edge(1, 2, 30)]
+        self.assertEqual(algorithm.hamiltonian_cycle, expected_hamiltonian_cycle)
+        weight = sum(edge.weight for edge in algorithm.hamiltonian_cycle)
+        self.assertEqual(weight, self.best_weight)
+
+    def test_sorted_edge_with_cycle_graph(self):
+        algorithm = SortedEdgeTSPWithGraph(self.G)
+        algorithm.run(0)
+        weight = sum(edge.weight for edge in algorithm.hamiltonian_cycle.iteredges())
+        self.assertEqual(weight, self.best_weight)
+        #algorithm.hamiltonian_cycle.show()
+        self.assertEqual(algorithm.hamiltonian_cycle.e(),
+                         algorithm.hamiltonian_cycle.v())
+        for node in algorithm.hamiltonian_cycle.iternodes():
+            self.assertEqual(algorithm.hamiltonian_cycle.degree(node), 2)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
---
+++ graphtheory/hamiltonian/tspse.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+
+from Queue import PriorityQueue
+from graphtheory.structures.unionfind import UnionFind
+
+
+class SortedEdgeTSPWithEdges:
+    """The sorted edge algorithm (a.k.a. cheapest link) for TSP.
+    
+    Attributes
+    ----------
+    graph : input weighted complete graph
+    hamiltonian_cycle : list of edges
+    _uf : disjoint-set data structure, private
+    _pq : priority queue, private
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.hamiltonian_cycle = list()
+        self._uf = UnionFind()
+        self._pq = PriorityQueue()
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        degree_dict = dict((node, 0) for node in self.graph.iternodes())
+        for node in self.graph.iternodes():
+            self._uf.create(node)
+        for edge in self.graph.iteredges():
+            self._pq.put((edge.weight, edge))
+        while not self._pq.empty():
+            _, edge = self._pq.get()
+            degree1 = degree_dict[edge.source]
+            degree2 = degree_dict[edge.target]
+            # Zabezpieczam przed skrzyzowaniami T, X, itp.
+            if degree1 == 2 or degree2 == 2:
+                continue
+            if degree1 == 0 or degree2 == 0:
+                self._uf.union(edge.source, edge.target)
+                self.hamiltonian_cycle.append(edge)
+                degree_dict[edge.source] += 1
+                degree_dict[edge.target] += 1
+                continue
+            # Here degree1 = degree2 = 1.
+            if self._uf.find(edge.source) != self._uf.find(edge.target):
+                # Join pieces.
+                self._uf.union(edge.source, edge.target)
+                self.hamiltonian_cycle.append(edge)
+                degree_dict[edge.source] += 1
+                degree_dict[edge.target] += 1
+            elif len(self.hamiltonian_cycle) == self.graph.v()-1:
+                # Close the Hamiltonian cycle.
+                self.hamiltonian_cycle.append(edge)
+                degree_dict[edge.source] += 1
+                degree_dict[edge.target] += 1
+        # Create the corrected (directed) list of edges.
+        # Przeksztalcam cykl Hamiltona do postaci poprawnej listy krawedzi.
+        # Na razie krawedzie moga miec zla kolejnosc i kierunek.
+        # Tworze slownik z krawedziami w obie strony.
+        edge_dict = dict((node, []) for node in self.graph.iternodes())
+        for edge in self.hamiltonian_cycle:   # O(V) time
+            edge_dict[edge.source].append(edge)
+            edge_dict[edge.target].append(~edge)
+        edge = self.hamiltonian_cycle[0]
+        self.hamiltonian_cycle = [edge]
+        # Kompletuje kolejne krawedzie.
+        for step in xrange(self.graph.v()-1):   # O(V) time
+            edge1, edge2 = edge_dict[edge.target]
+            if edge1.target == edge.source:
+                self.hamiltonian_cycle.append(edge2)
+                edge = edge2
+            else:
+                self.hamiltonian_cycle.append(edge1)
+                edge = edge1
+
+
+class SortedEdgeTSPWithGraph:
+    """The sorted edge algorithm for TSP.
+    
+    Attributes
+    ----------
+    graph : input weighted complete graph
+    hamiltonian_cycle : cycle graph
+    _uf : disjoint-set data structure, private
+    _pq : priority queue, private
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.hamiltonian_cycle = self.graph.__class__(self.graph.v())
+        for node in self.graph.iternodes():
+            self.hamiltonian_cycle.add_node(node)
+        self._uf = UnionFind()
+        self._pq = PriorityQueue()
+
+    def run(self, source=None):
+        """Executable pseudocode."""
+        for node in self.graph.iternodes():
+            self._uf.create(node)
+        for edge in self.graph.iteredges():
+            self._pq.put((edge.weight, edge))
+        while not self._pq.empty():
+            _, edge = self._pq.get()
+            degree1 = self.hamiltonian_cycle.degree(edge.source)
+            degree2 = self.hamiltonian_cycle.degree(edge.target)
+            # Zabezpieczam przed skrzyzowaniami T, X, itp.
+            if degree1 == 2 or degree2 == 2:
+                continue
+            if degree1 == 0 or degree2 == 0:
+                self._uf.union(edge.source, edge.target)
+                self.hamiltonian_cycle.add_edge(edge)
+                continue
+            # Here degree1 = degree2 = 1.
+            if self._uf.find(edge.source) != self._uf.find(edge.target):
+                # Join pieces.
+                self._uf.union(edge.source, edge.target)
+                self.hamiltonian_cycle.add_edge(edge)
+            elif self.hamiltonian_cycle.e() == self.hamiltonian_cycle.v()-1:
+                # Close the Hamiltonian cycle.
+                self.hamiltonian_cycle.add_edge(edge)
+
+# EOF
