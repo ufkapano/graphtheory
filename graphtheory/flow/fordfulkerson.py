b83ad062f7992ce2352abb85208e7b343384df9f
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2017-02-24 11:10:08 +0100

    FordFulkersonRecursive added.

--- graphtheory/flow/fordfulkerson.py
+++ graphtheory/flow/fordfulkerson.py
@@ -61,8 +61,8 @@ class FordFulkerson:
             target = self.sink
             while target != self.source:
                 node = parent[target]
-                self.flow[node][target] = self.flow[node][target] + min_capacity
-                self.flow[target][node] = self.flow[target][node] - min_capacity
+                self.flow[node][target] += min_capacity
+                self.flow[target][node] -= min_capacity
                 target = node
 
     def find_path(self):   # use DFS
@@ -117,14 +117,16 @@ class FordFulkersonSparse:
         self.residual = self.graph.__class__(self.graph.v(), directed=True)
         for node in self.graph.iternodes():
             self.residual.add_node(node)
-        # Initial capacities for the residual network.
-        for edge in self.graph.iteredges():
-            self.residual.add_edge(edge)   # original capacity
-            self.residual.add_edge(Edge(edge.target, edge.source, 0))
         # Legal flow.
         self.flow = dict()
         for node in self.graph.iternodes():
             self.flow[node] = dict()
+        # Initial capacities for the residual network.
+        for edge in self.graph.iteredges():
+            self.residual.add_edge(edge)   # original capacity
+            self.residual.add_edge(Edge(edge.target, edge.source, 0))
+            self.flow[edge.source][edge.target] = 0
+            self.flow[edge.target][edge.source] = 0
         # Initial flow is zero.
         self.max_flow = 0
 
@@ -141,8 +143,8 @@ class FordFulkersonSparse:
             target = self.sink
             while target != self.source:
                 node = parent[target]
-                self.flow[node][target] = self.flow[node].get(target, 0) + min_capacity
-                self.flow[target][node] = self.flow[target].get(node, 0) - min_capacity
+                self.flow[node][target] += min_capacity
+                self.flow[target][node] -= min_capacity
                 target = node
 
     def find_path(self):   # use DFS
@@ -155,7 +157,7 @@ class FordFulkersonSparse:
         while not Q.empty():
             node = Q.get()
             for edge in self.residual.iteroutedges(node):
-                cap = edge.weight - self.flow[edge.source].get(edge.target,0)
+                cap = edge.weight - self.flow[edge.source][edge.target]
                 if cap > 0 and parent[edge.target] is None:
                     parent[edge.target] = edge.source
                     capacity[edge.target] = min(capacity[edge.source], cap)
@@ -173,7 +175,8 @@ class FordFulkersonWithEdges:
     ----------
     graph : input directed graph (flow network)
     residual : directed graph (residual network)
-    flow : dict-of-dict
+    flow : dict with pairs (edge, number)
+    mate : dict with pairs (edge, edge)
     source : node
     sink : node
     max_flow : number
@@ -226,8 +229,8 @@ class FordFulkersonWithEdges:
             while target != self.source:
                 edge = parent[target]
                 edge2 = self.mate[edge]
-                self.flow[edge2] = self.flow[edge2] + min_capacity
-                self.flow[edge] = self.flow[edge] - min_capacity
+                self.flow[edge2] += min_capacity
+                self.flow[edge] -= min_capacity
                 target = edge.target
 
     def find_path(self):   # use DFS
@@ -249,5 +252,85 @@ class FordFulkersonWithEdges:
                     else:
                         return capacity[self.sink], parent
         return 0, parent
+
+
+class FordFulkersonRecursive:
+    """The Ford-Fulkerson algorithm for computing the maximum flow.
+    
+    Attributes
+    ----------
+    graph : input directed graph (flow network)
+    residual : directed graph (residual network)
+    flow : dict with pairs (edge, number)
+    mate : dict with pairs (edge, edge)
+    source : node
+    sink : node
+    max_flow : number
+    
+    Notes
+    -----
+    Based on:
+    
+    Cormen, T. H., Leiserson, C. E., Rivest, R. L., and Stein, C., 2009, 
+        Introduction to Algorithms, third edition, The MIT Press, 
+        Cambridge, London.
+    
+    https://en.wikipedia.org/wiki/Ford-Fulkerson_algorithm
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if not graph.is_directed():
+            raise ValueError("the graph is not directed")
+        self.graph = graph
+        self.residual = self.graph.__class__(self.graph.v(), directed=True)
+        for node in self.graph.iternodes():
+            self.residual.add_node(node)
+        # Legal flow.
+        self.flow = dict()
+        self.mate = dict()   # krawedz przeciwna w residual
+        # Initial capacities for the residual network.
+        for edge in self.graph.iteredges():
+            edge2 = Edge(edge.target, edge.source, 0)
+            self.residual.add_edge(edge)   # original capacity
+            self.residual.add_edge(edge2)
+            self.flow[edge] = 0
+            self.flow[edge2] = 0
+            self.mate[edge] = edge2
+            self.mate[edge2] = edge
+        # Initial flow is zero.
+        self.max_flow = 0
+
+    def run(self, source, sink):
+        """Executable pseudocode."""
+        self.source = source
+        self.sink = sink
+        path = self.find_path(self.source, self.sink, [])
+        while path:
+            min_capacity = min(edge.weight - self.flow[edge] for edge in path)
+            for edge in path:
+                edge2 = self.mate[edge]
+                self.flow[edge] += min_capacity
+                self.flow[edge2] -= min_capacity
+            path = self.find_path(self.source, self.sink, [])
+        self.max_flow = sum(self.flow[edge]
+            for edge in self.residual.iteroutedges(self.source))
+
+    def find_path(self, source, sink, path):   # use DFS
+        """Finding augmenting paths in the residual network."""
+        # 'path' contains edges.
+        if source == sink:
+            return path
+        for edge in self.residual.iteroutedges(source):
+            cap = edge.weight - self.flow[edge]
+            if cap > 0 and edge not in path and self.mate[edge] not in path:
+                # Sprawdzanie nalezenia do listy nie jest szybkie.
+                # Sprawdzamy, czy nie plynelismy ta krawedzia
+                # w jedna lub w druga strone.
+                new_path = self.find_path(edge.target, sink, path + [edge])
+                # Tu mamy kopiowanie dotychczasowej listy do funkcji.
+                if new_path:
+                    return new_path
+        return None
 
 # EOF
--- graphtheory/flow/tests/test_fordfulkerson.py
+++ graphtheory/flow/tests/test_fordfulkerson.py
@@ -6,6 +6,7 @@ from graphtheory.structures.graphs import Graph
 from graphtheory.flow.fordfulkerson import FordFulkerson
 from graphtheory.flow.fordfulkerson import FordFulkersonSparse
 from graphtheory.flow.fordfulkerson import FordFulkersonWithEdges
+from graphtheory.flow.fordfulkerson import FordFulkersonRecursive
 
 #     10
 #  0 ---> 1
@@ -47,15 +48,32 @@ class TestFordFulkerson(unittest.TestCase):
         algorithm.run(0, 3)
         expected_max_flow = 20
         expected_flow = {
-            0: {2: 10, 1: 10}, 
+            0: {1: 10, 2: 10},
+            1: {0: -10, 2: 0, 3: 10},
+            2: {0: -10, 1: 0, 3: 10},
+            3: {1: -10, 2: -10}}
+        expected_flow2 = {
+            0: {1: 10, 2: 10}, 
             1: {0: -10, 3: 10}, 
             2: {0: -10, 3: 10}, 
-            3: {2: -10, 1: -10}}
+            3: {1: -10, 2: -10}}
         self.assertEqual(algorithm.max_flow, expected_max_flow)
         self.assertEqual(algorithm.flow, expected_flow)
 
     def test_fordfulkerson_with_edges(self):
         algorithm = FordFulkersonWithEdges(self.G)
+        algorithm.run(0, 3)
+        expected_max_flow = 20
+        expected_flow = {
+            Edge(0, 1, 10): 10, Edge(0, 2, 10): 10,
+            Edge(1, 0, 0): -10, Edge(1, 2): 0, Edge(1, 3, 10): 10,
+            Edge(2, 0, 0): -10, Edge(2, 1, 0): 0, Edge(2, 3, 10): 10,
+            Edge(3, 1, 0): -10, Edge(3, 2, 0): -10}
+        self.assertEqual(algorithm.max_flow, expected_max_flow)
+        self.assertEqual(algorithm.flow, expected_flow)
+
+    def test_fordfulkerson_recursive(self):
+        algorithm = FordFulkersonRecursive(self.G)
         algorithm.run(0, 3)
         expected_max_flow = 20
         expected_flow = {
@@ -103,6 +121,14 @@ class TestFordFulkersonWiki(unittest.TestCase):
         algorithm.run(0, 6)
         expected_max_flow = 5
         expected_flow = {
+            0: {1: 2, 2: 0, 3: 3},
+            1: {0: -2, 2: 2, 4: 0},
+            2: {0: 0, 1: -2, 3: 1, 4: 1},
+            3: {0: -3, 2: -1, 4: 0, 5: 4},
+            4: {1: 0, 2: -1, 3: 0, 6: 1},
+            5: {3: -4, 6: 4},
+            6: {4: -1, 5: -4}}
+        expected_flow2 = {
             0: {1: 2, 3: 3}, 
             1: {0: -2, 2: 2}, 
             2: {1: -2, 4: 1, 3: 1}, 
@@ -126,6 +152,29 @@ class TestFordFulkersonWiki(unittest.TestCase):
             Edge(3, 4, 2): 0, Edge(3, 5, 6): 4,
             Edge(4, 1): 0, Edge(4, 2, 0): -1,
             Edge(4, 3, 0): 0, Edge(4, 6): 1,
+            Edge(5, 3, 0): -4, Edge(5, 6, 9): 4,
+            Edge(6, 4, 0): -1, Edge(6, 5, 0): -4}
+        self.assertEqual(algorithm.max_flow, expected_max_flow)
+        self.assertEqual(algorithm.flow, expected_flow)
+
+    def test_wiki_recursive(self):
+        algorithm = FordFulkersonRecursive(self.G)
+        algorithm.run(0, 6)
+        expected_max_flow = 5
+        expected_flow = {
+            Edge(0, 1, 3): 3, Edge(0, 2, 0): -1, Edge(0, 3, 3): 3,
+            Edge(1, 0, 0): -3, Edge(1, 2, 4): 3, Edge(1, 4, 0): 0,
+            Edge(2, 0, 3): 1, Edge(2, 1, 0): -3, Edge(2, 3): 1, Edge(2, 4, 2): 1,
+            Edge(3, 0, 0): -3, Edge(3, 2, 0): -1, Edge(3, 4, 2): 0, Edge(3, 5, 6): 4,
+            Edge(4, 1): 0, Edge(4, 2, 0): -1, Edge(4, 3, 0): 0, Edge(4, 6): 1,
+            Edge(5, 3, 0): -4, Edge(5, 6, 9): 4,
+            Edge(6, 4, 0): -1, Edge(6, 5, 0): -4}
+        expected_flow2 = {
+            Edge(0, 1, 3): 2, Edge(0, 2, 0): 0, Edge(0, 3, 3): 3,
+            Edge(1, 0, 0): -2, Edge(1, 2, 4): 2, Edge(1, 4, 0): 0,
+            Edge(2, 0, 3): 0, Edge(2, 1, 0): -2, Edge(2, 3): 1, Edge(2, 4, 2): 1,
+            Edge(3, 0, 0): -3, Edge(3, 2, 0): -1, Edge(3, 4, 2): 0, Edge(3, 5, 6): 4,
+            Edge(4, 1): 0, Edge(4, 2, 0): -1, Edge(4, 3, 0): 0, Edge(4, 6): 1,
             Edge(5, 3, 0): -4, Edge(5, 6, 9): 4,
             Edge(6, 4, 0): -1, Edge(6, 5, 0): -4}
         self.assertEqual(algorithm.max_flow, expected_max_flow)
