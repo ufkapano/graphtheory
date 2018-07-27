940b9e2726b658362c9e3b959b43d2189a962210
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2018-07-27 17:13:52 +0200

    spnodecolor added.

---
+++ graphtheory/seriesparallel/spnodecolor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from graphtheory.bipartiteness.bipartite import BipartiteGraphBFS
+from graphtheory.connectivity.connected import is_connected
+from graphtheory.seriesparallel.sptools import find_peo_spgraph
+
+
+class SPNodeColoring:
+    """Find sp-graph node coloring."""
+    # Kolory sa kolejnymi liczbami 0, 1, 2, ...
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        if not is_connected(graph):
+            raise ValueError("the graph is not connected")
+        self.graph = graph
+        self.color = dict((node, None) for node in self.graph.iternodes())
+        self._color_list = [False] * self.graph.v()
+
+    def run(self):
+        """Executable pseudocode."""
+        try:
+            algorithm = BipartiteGraphBFS(self.graph)
+            algorithm.run()
+            self.color = algorithm.color
+        except ValueError:
+            order = find_peo_spgraph(self.graph)
+            for source in reversed(order):
+                self._greedy_color(source)
+
+    def _greedy_color(self, source):
+        """Give node the smallest possible color."""
+        for target in self.graph.iteradjacent(source):
+            if self.color[target] is not None:
+                self._color_list[self.color[target]] = True
+        for c in xrange(self.graph.v()):   # check colors
+            if not self._color_list[c]:
+                self.color[source] = c
+                break
+        for target in self.graph.iteradjacent(source):
+            if self.color[target] is not None:
+                self._color_list[self.color[target]] = False
+        return c
+
+# EOF
---
+++ graphtheory/seriesparallel/tests/test_spnodecolor.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.structures.factory import GraphFactory
+from graphtheory.seriesparallel.spnodecolor import SPNodeColoring
+
+# 0 --- 1 --- 2 nie jest dwudzielny, bo sa trojkaty
+# |   / |   /
+# |  /  |  /
+# | /   | /
+# 3 --- 4
+
+class TestNodeColoring(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 5
+        self.G = Graph(self.N)
+        self.nodes = range(self.N)
+        self.edges = [Edge(0, 1), Edge(0, 3), Edge(1, 3), Edge(1, 4),
+            Edge(1, 2), Edge(2, 4), Edge(3, 4)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+        #self.G.show()
+
+    def test_spgraph_node_coloring(self):
+        algorithm = SPNodeColoring(self.G)
+        algorithm.run()
+        # Sprawdzenie, czy kazdy wierzcholek ma kolor.
+        for node in self.G.iternodes():
+            self.assertNotEqual(algorithm.color[node], None)
+        for edge in self.G.iteredges():
+            self.assertNotEqual(algorithm.color[edge.source],
+                                algorithm.color[edge.target])
+        all_colors = set(algorithm.color[node] for node in self.G.iternodes())
+        self.assertEqual(len(all_colors), 3)
+
+    def test_cycle_node_coloring(self):
+        gf = GraphFactory(Graph)
+        self.G = gf.make_cyclic(2 * self.N)   # bipartite
+        algorithm = SPNodeColoring(self.G)
+        algorithm.run()
+        # Sprawdzenie, czy kazdy wierzcholek ma kolor.
+        for node in self.G.iternodes():
+            self.assertNotEqual(algorithm.color[node], None)
+        for edge in self.G.iteredges():
+            self.assertNotEqual(algorithm.color[edge.source],
+                                algorithm.color[edge.target])
+        all_colors = set(algorithm.color[node] for node in self.G.iternodes())
+        self.assertEqual(len(all_colors), 2)
+
+    def test_exceptions(self):
+        self.assertRaises(ValueError, SPNodeColoring,
+            Graph(5, directed=True))
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
