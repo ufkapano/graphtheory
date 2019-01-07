ca53cf61cd318f1cb6833e1a4d933bf0d396ae51
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2019-01-07 13:01:28 +0100

    SLFNodeColoring added.

---
+++ graphtheory/coloring/nodecolorslf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+class SLFNodeColoring:
+    """Find a saturated largest first (SLF) node coloring.
+    Computational complexity is O(V^2).
+    
+    Attributes
+    ----------
+    graph : input undirected graph or multigraph
+    color : dict with nodes (values are colors)
+    
+    Notes
+    -----
+    Colors are 0, 1, 2, ...
+    """
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.color = dict((node, None) for node in self.graph.iternodes())
+        for edge in self.graph.iteredges():
+            if edge.source == edge.target:
+                raise ValueError("a loop detected")
+        # Saturacja moze sie przydac po zakonczeniu algorytmu.
+        self.saturation = dict((node, set()) for node in self.graph.iternodes())
+
+    def run(self):
+        """Executable pseudocode."""
+        n = self.graph.v()
+        for step in xrange(n):
+            source = max((node for node in self.graph.iternodes()
+                if self.color[node] is None), key=lambda x:
+                n * len(self.saturation[x]) + self.graph.degree(x))
+            self._greedy_color_with_saturation(source)
+
+    def _greedy_color_with_saturation(self, source):
+        """Give node the smallest possible color."""
+        for c in xrange(self.graph.v()):
+            if c not in self.saturation[source]:
+                self.color[source] = c
+                break
+        # Sasiadom podwyzszamy stopien nasycenia dodajac kolory.
+        for edge in self.graph.iteroutedges(source):
+            self.saturation[edge.target].add(c)
+        return c
+
+# EOF
---
+++ graphtheory/coloring/tests/test_nodecolorslf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.coloring.nodecolorslf import SLFNodeColoring
+
+# 0 --- 1 --- 2   outerplanar, chordal, 2-tree
+# |   / |   / |
+# |  /  |  /  |
+# | /   | /   |
+# 3 --- 4 --- 5
+# Best node coloring - 3 colors.
+# color = {0:a, 1:b, 2:c, 3:c, 4:a, 5:b}
+
+class TestNodeColoring(unittest.TestCase):
+
+    def setUp(self):
+        self.N = 6
+        self.G = Graph(self.N)
+        self.nodes = range(self.N)
+        self.edges = [
+            Edge(0, 1), Edge(0, 3), Edge(1, 3), Edge(1, 4), Edge(1, 2), 
+            Edge(2, 4), Edge(2, 5), Edge(3, 4), Edge(4, 5)]
+        for node in self.nodes:
+            self.G.add_node(node)
+        for edge in self.edges:
+            self.G.add_edge(edge)
+        #self.G.show()
+
+    def test_slf_node_coloring(self):
+        algorithm = SLFNodeColoring(self.G)
+        algorithm.run()
+        for node in self.G.iternodes():
+            self.assertNotEqual(algorithm.color[node], None)
+        for edge in self.G.iteredges():
+            self.assertNotEqual(algorithm.color[edge.source],
+                                algorithm.color[edge.target])
+        #print algorithm.color
+        all_colors = set(algorithm.color[node] for node in self.G.iternodes())
+        self.assertEqual(len(all_colors), 3)
+
+    def test_exceptions(self):
+        self.assertRaises(ValueError, SLFNodeColoring,
+            Graph(5, directed=True))
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
