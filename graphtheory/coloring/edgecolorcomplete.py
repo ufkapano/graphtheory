5c6c6afa9170e4ad38feedf18ffcb98f7e77b177
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2019-07-30 11:21:16 +0200

    edgecolorcomplete added.

---
+++ graphtheory/coloring/edgecolorcomplete.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+try:
+    integer_types = (int, long)
+except NameError:   # Python 3
+    integer_types = (int,)
+    xrange = range
+
+
+class CompleteGraphEdgeColoring:
+    """Find an edge coloring for a complete graph."""
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.color = dict()
+        self.m = 0   # graph.e() is slow
+        for edge in self.graph.iteredges():
+            if edge.source == edge.target:
+                raise ValueError("a loop detected")
+            else:
+                self.color[edge] = None   # edge.source < edge.target
+                self.m += 1
+        if len(self.color) < self.m:
+            raise ValueError("edges are not unique")
+
+    def run(self):
+        """Executable pseudocode."""
+        if self.graph.v() % 2 == 1:
+            self.run_odd()
+        else:
+            self.run_even()
+
+    def run_odd(self):
+        """Edge coloring for n odd (n colors) in O(n^2) time."""
+        n = self.graph.v()
+        # Node numbering.
+        D = dict((node, i) for (i, node) in enumerate(self.graph.iternodes()))   # O(n)
+        # Edge coloring.
+        for edge in self.graph.iteredges():   # O(E)=O(n^2) time
+            c = (D[edge.source] + D[edge.target]) % n
+            self.color[edge] = c
+
+    def run_even(self):
+        """Edge coloring for n even (n-1 colors)."""
+        n = self.graph.v()
+        removed_node = next(self.graph.iternodes())
+        removed_edges = list(self.graph.iteroutedges(removed_node)) # O(n) time
+        self.graph.del_node(removed_node) # O(n) time, removed with edges
+        self.run_odd()
+        # Find missing colors.
+        free = dict((node, set(xrange(n-1))) for node in self.graph.iternodes())
+        for edge in self.graph.iteredges():   # O(E)=O(n^2) time
+            c = self.color[edge]
+            free[edge.source].remove(c)
+            free[edge.target].remove(c)
+        # Restoring nodes and edges.
+        self.graph.add_node(removed_node)
+        for edge in removed_edges:   # O(n) time
+            assert edge.source == removed_node
+            c = free[edge.target].pop()
+            assert len(free[edge.target]) == 0   # only one color was free
+            self.graph.add_edge(edge)
+            if edge.source > edge.target:
+                edge = ~edge
+            self.color[edge] = c
+
+    def _get_color(self, edge):
+        """Get edge color."""
+        if edge.source > edge.target:
+            edge = ~edge
+        return self.color[edge]
+
+    def show_colors(self):
+        """Show edge coloring (undirected graphs)."""
+        L = []
+        for source in self.graph.iternodes():
+            L.append("{} : ".format(source))
+            for edge in self.graph.iteroutedges(source):
+                # It should work for multigraphs.
+                c = self._get_color(edge)
+                L.append("{}({}) ".format(edge.target, c))
+            L.append("\n")
+        print("".join(L))
+
+# EOF
---
+++ graphtheory/coloring/tests/test_edgecolorcomplete.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.structures.factory import GraphFactory
+from graphtheory.coloring.edgecolorcomplete import CompleteGraphEdgeColoring
+
+class TestEdgeColoring(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_Kn_odd(self):
+        N = 7
+        self.assertEqual(N % 2, 1)
+        gf = GraphFactory(Graph)
+        G = gf.make_complete(N)
+        algorithm = CompleteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        self.assertEqual(len(all_colors), N)
+
+    def test_Kn_even(self):
+        N = 10
+        self.assertEqual(N % 2, 0)
+        gf = GraphFactory(Graph)
+        G = gf.make_complete(N)
+        algorithm = CompleteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        self.assertEqual(len(all_colors), N-1)
+
+    def test_exceptions(self):
+        self.assertRaises(ValueError, CompleteGraphEdgeColoring,
+            Graph(5, directed=True))
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
