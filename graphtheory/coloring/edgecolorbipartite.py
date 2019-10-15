aeb68c01b9c5895f9f0b7e11c015361d4d112fd6
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2019-10-15 16:28:52 +0200

    edgecolorbipartite added.

---
+++ graphtheory/coloring/edgecolorbipartite.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+
+try:
+    integer_types = (int, long)
+except NameError:   # Python 3
+    integer_types = (int,)
+    xrange = range
+
+from graphtheory.bipartiteness.bipartite import BipartiteGraphBFS as Bipartite
+#from graphtheory.bipartiteness.bipartite import BipartiteGraphDFS as Bipartite
+from graphtheory.coloring.edgecolorcs import ConnectedSequentialEdgeColoring
+
+
+class CompleteBipartiteGraphEdgeColoring:
+    """Find an edge coloring for a complete bipartite graph."""
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.color = dict()
+        self.m = 0   # graph.e() is slow
+        for edge in self.graph.iteredges():
+            if edge.source == edge.target:
+                raise ValueError("a loop detected")
+            else:
+                self.color[edge] = None   # edge.source < edge.target
+                self.m += 1
+        if len(self.color) < self.m:
+            raise ValueError("edges are not unique")
+        algorithm = Bipartite(self.graph)   # O(V+E) time
+        algorithm.run()
+        # Dictionaries for node indices.
+        self.D1 = dict()
+        self.D2 = dict()
+        idx1 = 0
+        idx2 = 0
+        for node in self.graph.iternodes():   # O(V) time
+            if algorithm.color[node] == 1:
+                self.D1[node] = idx1
+                idx1 += 1
+            else:
+                self.D2[node] = idx2
+                idx2 += 1
+        if self.m != len(self.D1) * len(self.D2):
+            raise ValueError("the graph is not complete bipartite")
+
+    def run(self):
+        """Executable pseudocode."""
+        # The number of available edge colors.
+        Delta = max(len(self.D1), len(self.D2))
+        for node in self.D1:   # O(E) time
+            for edge in self.graph.iteroutedges(node):
+                # Edge targets are in D2.
+                c = (self.D1[edge.source] + self.D2[edge.target]) % Delta
+                if edge.source > edge.target:   # edges can be reversed
+                    edge = ~edge
+                self.color[edge] = c
+
+    def _get_color(self, edge):
+        """Get color."""
+        if edge.source > edge.target:
+            edge = ~edge
+        return self.color[edge]
+
+    def show_colors(self):
+        """Show edge coloring (undirected graphs)."""
+        L = []
+        for source in self.graph.iternodes():
+            L.append("{} : ".format(source))
+            for edge in self.graph.iteroutedges(source):
+                # It should work for multigraphs.
+                c = self._get_color(edge)
+                L.append("{}({}) ".format(edge.target, c))
+            L.append("\n")
+        print("".join(L))
+
+
+class BipartiteGraphEdgeColoring:
+    """Find an edge coloring for a bipartite graph."""
+    # Szacowanie zlozonosci obliczeniowej.
+    # Na poczatku O(V+E) na rozpoznanie grafu dwudzielnego.
+    # E krawedzi do kolorowania.
+    # O(V) na ustalenie Delta.
+    # Tworzenie slownika missing to O(V*Delta).
+    # O(Delta) na znalezienie wspolnego koloru brakujacego.
+    # Jezeli nie ma wspolnego koloru brakujacego, to:
+    # O(V) na znalezienie sciezki,
+    # O(V) na przekolorowanie sciezki.
+    # Overall, O(V*E) complexity.
+
+    def __init__(self, graph):
+        """The algorithm initialization."""
+        if graph.is_directed():
+            raise ValueError("the graph is directed")
+        self.graph = graph
+        self.color = dict()
+        self.m = 0   # graph.e() is slow
+        for edge in self.graph.iteredges():
+            if edge.source == edge.target:
+                raise ValueError("a loop detected")
+            else:
+                self.color[edge] = None   # edge.source < edge.target
+                self.m += 1
+        if len(self.color) < self.m:
+            raise ValueError("edges are not unique")
+        # Tast czy graf jest dwudzielny.
+        # Wlasciwie potem nie jest jawnie potrzebny podzial wierzcholkow.
+        algorithm = Bipartite(self.graph)   # O(V+E) time
+        algorithm.run()
+        # dict with missing colors for nodes.
+        self.missing = None
+
+    def run(self):
+        """Executable pseudocode."""
+        # Ustal liczbe wykorzystywanych kolorow.
+        Delta = max(self.graph.degree(node) for node in self.graph.iternodes())
+        if Delta <= 2:
+            # Greedy coloring suffies.
+            algorithm = ConnectedSequentialEdgeColoring(self.graph)
+            algorithm.run()
+            self.color = algorithm.color
+        else:
+            self.missing = dict((node, set(xrange(Delta)))
+                for node in self.graph.iternodes())
+            for edge in self.graph.iteredges():
+                # Sprawdz wspolny kolor brakujacy.
+                # To mozna chyba zrobic bardziej wydajnie.
+                both = self.missing[edge.source] & self.missing[edge.target]
+                if len(both) == 0:
+                    self._recolor(edge)
+                else:
+                    c = min(both)   # choose min color available [moze dowolny?]
+                    self._add_color(edge, c)
+
+    def _add_color(self, edge, c):
+        """Add color."""
+        if edge.source > edge.target:
+            edge = ~edge
+        self.color[edge] = c
+        self.missing[edge.source].remove(c)
+        self.missing[edge.target].remove(c)
+
+    def _del_color(self, edge, c):
+        """Delete color."""
+        if edge.source > edge.target:
+            edge = ~edge
+        self.color[edge] = None
+        self.missing[edge.source].add(c)
+        self.missing[edge.target].add(c)
+
+    def _get_color(self, edge):
+        """Get color."""
+        if edge.source > edge.target:
+            edge = ~edge
+        return self.color[edge]
+
+    def show_colors(self):
+        """Show edge coloring (undirected graphs)."""
+        L = []
+        for source in self.graph.iternodes():
+            L.append("{} : ".format(source))
+            for edge in self.graph.iteroutedges(source):
+                # It should work for multigraphs.
+                c = self._get_color(edge)
+                L.append("{}({}) ".format(edge.target, c))
+            L.append("\n")
+        print("".join(L))
+
+    def _recolor(self, edge):
+        """Swap edge colors and add color."""
+        # edge.source i edge.target maja rozne kolory brakujace.
+        alpha = min(self.missing[edge.source])
+        beta = min(self.missing[edge.target])
+        # Tworze sciezke o poczatku w edge.source i kolorach
+        # na przemian beta i alpha.
+        # Sciezka sie urwie, jak nie znajdziemy danego koloru.
+        # Na sciezce na pewno nie spotkamy edge.target, bo tam
+        # nie ma koloru beta.
+        path = []
+        node = edge.source   # chodzi po wierzcholkach sciezki
+        finished = False
+        # Zmienna parity pozwala kontrolowac jaki kolor szukamy.
+        parity = 0
+        while not finished:
+            finished = True
+            if parity % 2 == 0:   # szukamy kolor beta
+                for edge1 in self.graph.iteroutedges(node):
+                    # Kolor krawedzi ma byc beta.
+                    c = self._get_color(edge1)
+                    if c == beta:   # c moze byc None!
+                        node = edge1.target
+                        path.append(edge1)
+                        finished = False   # bedziemy szukac drugiego koloru
+                        break
+            else:   # parity % 2 == 1, szukamy kolor alpha
+                for edge1 in self.graph.iteroutedges(node):
+                    # Kolor krawedzi ma byc alpha.
+                    c = self._get_color(edge1)
+                    if c == alpha:   # c moze byc None!
+                        node = edge1.target
+                        path.append(edge1)
+                        finished = False   # bedziemy szukac drugiego koloru
+                        break
+            parity += 1
+        #print "path", path
+        # Sciezka zostala znaleziona i na pewno istnieje.
+        # Zamieniamy kolory alpha i beta na sciezce.
+        # Najpierw usuwam kolory, pierwszy to beta.
+        for i, edge1 in enumerate(path):
+            c = beta if (i % 2 == 0) else alpha
+            self._del_color(edge1, c)
+        # Teraz dodaje kolory, pierwszy to alpha.
+        for i, edge1 in enumerate(path):
+            c = alpha if (i % 2 == 0) else beta
+            self._add_color(edge1, c)
+        # Teraz mamy wolny kolor beta dla krawedzi edge.
+        self._add_color(edge, beta)
+
+# EOF
---
+++ graphtheory/coloring/tests/test_edgecolorbipartite.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.structures.factory import GraphFactory
+from graphtheory.coloring.edgecolorbipartite import CompleteBipartiteGraphEdgeColoring
+from graphtheory.coloring.edgecolorbipartite import BipartiteGraphEdgeColoring
+
+
+class TestEdgeColoring(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_Kpq(self):
+        N1 = 5
+        N2 = 3
+        gf = GraphFactory(Graph)
+        G = gf.make_bipartite(N1, N2, False, 1)
+        self.assertFalse(G.is_directed())
+        self.assertEqual(G.v(), N1 + N2)
+        self.assertEqual(G.e(), N1 * N2)
+        algorithm = CompleteBipartiteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        self.assertEqual(len(all_colors), max(N1, N2))
+
+    def test_exceptions(self):
+        self.assertRaises(ValueError, CompleteBipartiteGraphEdgeColoring,
+            Graph(5, directed=True))
+        gf = GraphFactory(Graph)
+        G = gf.make_bipartite(2, 2, False, 1)
+        #G.show()
+        G.del_edge(Edge(0, 2))   # nie bedzie pelny dwudzielny
+        self.assertRaises(ValueError, CompleteBipartiteGraphEdgeColoring, G)
+
+    def tearDown(self): pass
+
+
+class TestBipartiteEdgeColoring(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_cyclic_graph(self):
+        N = 8
+        assert N % 2 == 0
+        gf = GraphFactory(Graph)
+        G = gf.make_cyclic(N)
+        algorithm = BipartiteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        self.assertEqual(len(all_colors), 2)
+
+    def test_Kpq(self):
+        N1 = 15
+        N2 = 13
+        gf = GraphFactory(Graph)
+        G = gf.make_bipartite(N1, N2, False, 1)
+        self.assertFalse(G.is_directed())
+        self.assertEqual(G.v(), N1 + N2)
+        self.assertEqual(G.e(), N1 * N2)
+        algorithm = BipartiteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        self.assertEqual(len(all_colors), max(N1, N2))
+
+    def test_bipartite(self):
+        N1 = 10
+        N2 = 13
+        gf = GraphFactory(Graph)
+        G = gf.make_bipartite(N1, N2)
+        algorithm = BipartiteGraphEdgeColoring(G)
+        algorithm.run()
+        for edge in G.iteredges():
+            self.assertNotEqual(algorithm.color[edge], None)
+        for node in G.iternodes():
+            color_set = set()
+            for edge in G.iteroutedges(node):
+                if edge.source > edge.target:
+                    color_set.add(algorithm.color[~edge])
+                else:
+                    color_set.add(algorithm.color[edge])
+            self.assertEqual(len(color_set), G.degree(node))
+        #print algorithm.color
+        #algorithm.show_colors()
+        all_colors = set(algorithm.color[edge] for edge in G.iteredges())
+        Delta = max(G.degree(node) for node in G.iternodes())
+        self.assertEqual(len(all_colors), Delta)
+
+    def test_exceptions(self):
+        self.assertRaises(ValueError, BipartiteGraphEdgeColoring,
+            Graph(5, directed=True))
+        gf = GraphFactory(Graph)
+        G = gf.make_cyclic(4)
+        #G.show()
+        G.add_edge(Edge(0, 2))   # nie bedzie dwudzielny
+        self.assertRaises(ValueError, BipartiteGraphEdgeColoring, G)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
