ab42c51b8fa2c4cdb8d5cfd0b7de4702824f22c9
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2021-07-10 15:27:26 +0200

    Random maximal outerplanar graphs.

---
+++ graphtheory/planarity/genouterplanar.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import random
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+
+class MaximalOuterplanarGenerator:
+    """Generator of maximal outerplanar graphs."""
+
+    def __init__(self, n):
+        if not isinstance(n, int):
+            raise ValueError("n is not int")
+        if n < 0:
+            raise ValueError("negative n")
+        self.n = n
+        self.graph = Graph()
+
+    def run(self):
+        # create hamiltonian cycle (cycle graph)
+        weights = list(range(1, 1 + 2 * self.n - 3))   # different weights
+        for i in range(self.n):
+            self.graph.add_edge( Edge(i, (i + 1) % self.n, weights.pop()) )
+        self._generate(weights)
+        return self.graph
+
+    def _generate(self, weights):
+        G = self.graph.copy()
+        node_list = list(G.iternodes())   # O(V) time
+        random.shuffle(node_list)   # O(V) time
+        while G.v() > 3:   # O(V) time
+            # fill cycle with random edges
+            node = node_list.pop()   # O(1) time
+            source, target = list(G.iteradjacent(node))
+            chord = Edge(source, target, weights.pop())
+            self.graph.add_edge(chord)
+            G.add_edge(chord)
+            G.del_node(node)   # with edges
+        assert weights == []
+
+# EOF
---
+++ graphtheory/planarity/tests/test_genouterplanar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import unittest
+from graphtheory.structures.edges import Edge
+from graphtheory.structures.graphs import Graph
+from graphtheory.planarity.genouterplanar import MaximalOuterplanarGenerator
+
+
+class TestOuterplanar(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_triangle(self):    # graf C3
+        n = 3
+        algorithm = MaximalOuterplanarGenerator(n)
+        algorithm.run()
+        self.assertEqual(algorithm.graph.v(), n)
+        self.assertEqual(algorithm.graph.e(), 2*n-3)
+        #algorithm.graph.show()
+
+    def test_2triangles(self):    # graf C3
+        n = 4
+        algorithm = MaximalOuterplanarGenerator(n)
+        algorithm.run()
+        self.assertEqual(algorithm.graph.v(), n)
+        self.assertEqual(algorithm.graph.e(), 2*n-3)
+        #algorithm.graph.show()
+
+    def test_3triangles(self):    # graf C3
+        n = 5
+        algorithm = MaximalOuterplanarGenerator(n)
+        algorithm.run()
+        self.assertEqual(algorithm.graph.v(), n)
+        self.assertEqual(algorithm.graph.e(), 2*n-3)
+        #algorithm.graph.show()
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
